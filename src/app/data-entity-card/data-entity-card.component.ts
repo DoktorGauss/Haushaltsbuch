8a97ca5eaa4236ffd4b593c50ade3a2fee7c8c83
Dr Gauss <heilmann.sebastian@live.de>
2019-04-13 20:28:54 +0200

    #779 Line in dynamic PastDiv

--- src/app/data-entity-card/data-entity-card.component.ts
+++ src/app/data-entity-card/data-entity-card.component.ts
@@ -12,67 +12,93 @@ import { PortalHostDirective } from '@angular/cdk/portal';
 })
 export class DataEntityCardComponent implements AfterViewInit {
 
-  @Input() dataEntityCard:DataEntityCard;
-  @ViewChild('PastDiv') PastDiv: ElementRef; 
+  @Input() dataEntityCard: DataEntityCard;
+  @ViewChild('PastDiv') PastDiv: ElementRef;
+  MyPastDivLines: HTMLDivElement[] = [];
   Value = 50;
   ProduktName = "Milch";
   DurchschnittValue = 50;
   UeberschriftValue = "PPS";
   UeberschriftDurchschnittsValue = "Durchschnitt";
   // @Output() click = new EventEmitter<DataEntityCard>();
-  
-  constructor(private   LineService : LineService , private dataEntityService: DataEntityService) {
-    
+
+  constructor(private LineService: LineService, private dataEntityService: DataEntityService) {
+
   }
 
-  ngAfterViewInit() 
-  {
-    if(this.dataEntityCard){
+  ngAfterViewInit() {
+    if (this.dataEntityCard) {
       this.Value = this.dataEntityCard.Value.PreisPS;
       this.ProduktName = this.dataEntityCard.Value.ProductName;
+      this.updatePastDiv();
+    }
+  }
+
+  private updatePastDiv() {
+    this.clearPastDivFromLines();
+    this.createChart();
+    this.InsertMittelwertLinie();
+  }
+
+  clearPastDivFromLines(): any {
+    for (let index = 0; index < this.MyPastDivLines.length; index++) {
+      const element = this.MyPastDivLines[index];
+      (this.PastDiv.nativeElement as HTMLDivElement).removeChild(element);
+    }
+    this.MyPastDivLines = [];
+  }
+
+  InsertMittelwertLinie(): any {
+
+    const div = (this.PastDiv.nativeElement as HTMLDivElement);
+    const height = div.clientHeight;
+    const width = div.clientWidth;
+    const line = this.LineService.createLineByType(0, height/2, width, height/2, 'dotted');
+    this.MyPastDivLines.push(line);
+    (this.PastDiv.nativeElement as HTMLDivElement).appendChild(line);
+  }
+
+  createChart(): any {
+    const dataEntityArray = this.dataEntityCard.PastValues;
+    const X = this.dataEntityService.createArray(dataEntityArray, "DataID");
+    const Y = this.dataEntityService.createArray(dataEntityArray, "PreisPS");
+
+    const PastDivLines: HTMLDivElement[] = this.LineService.createChart(this.PastDiv.nativeElement as HTMLDivElement, X, Y);
+    this.MyPastDivLines = PastDivLines;
+    for (let index = 0; index < PastDivLines.length; index++) {
+      const element = PastDivLines[index];
       // debugger;
-      const dataEntityArray = this.dataEntityCard.PastValues;
-      const X = this.dataEntityService.createArray(dataEntityArray, "DataID");
-      const Y = this.dataEntityService.createArray(dataEntityArray, "PreisPS");
-
-      const PastDivLines: HTMLDivElement[] =  this.LineService.createChart( this.PastDiv.nativeElement as HTMLDivElement, X, Y);
-      for (let index = 0; index < PastDivLines.length; index++) {
-        const element = PastDivLines[index];
-        // debugger;
-        (this.PastDiv.nativeElement as HTMLDivElement).appendChild(element);
-      }
-
-
-      // (this.PastDiv.nativeElement as HTMLDivElement).appendChild(this.LineService.createLine(0, 0, 0, 100));
-      // (this.PastDiv.nativeElement as HTMLDivElement).appendChild(this.LineService.createLine(0, 100, 100, 100));
-      // (this.PastDiv.nativeElement as HTMLDivElement).appendChild(this.LineService.createLine(100, 100, 100, 0));
-      // (this.PastDiv.nativeElement as HTMLDivElement).appendChild(this.LineService.createLine(100, 0, 0, 0));
+      (this.PastDiv.nativeElement as HTMLDivElement).appendChild(element);
     }
   }
 
 
 
-  public FutureDivExpands(e){
-    var tmp = e.currentTarget.parentElement.children;
-    
 
+
+  public FutureDivExpands(e) {
+    var tmp = e.currentTarget.parentElement.children;
     tmp[0].style.width = "50px";
     tmp[1].style.width = "50px";
     tmp[2].style.width = "200px";
+    this.updatePastDiv();
   }
 
-  NormalDiv(e){
+  NormalDiv(e) {
     var tmp = e.currentTarget.parentElement.children;
     tmp[0].style.width = "100px";
     tmp[1].style.width = "100px";
     tmp[2].style.width = "100px";
+    this.updatePastDiv();
   }
 
-  PastDivExpands(e){
+  PastDivExpands(e) {
     var tmp = e.currentTarget.parentElement.children;
     tmp[0].style.width = "200px";
     tmp[1].style.width = "50px";
     tmp[2].style.width = "50px";
+    debugger;
+    this.updatePastDiv();
   }
 
 
--- src/app/providers/line.service.ts
+++ src/app/providers/line.service.ts
@@ -70,7 +70,23 @@ export class LineService {
   }
   private createLineElement(x, y, length, angle): HTMLDivElement{
     var line = document.createElement("div");
-    var styles = 'border: 1pt solid blue; '
+    var styles = 'border: 0.5pt  solid black; '
+      + 'width: ' + length + 'px; '
+      + 'height: 0; '
+      + '-moz-transform: rotate(' + angle + 'rad); '
+      + '-webkit-transform: rotate(' + angle + 'rad); '
+      + '-o-transform: rotate(' + angle + 'rad); '
+      + '-ms-transform: rotate(' + angle + 'rad); '
+      + 'position: absolute; '
+      + 'top: ' + y + 'px; '
+      + 'left: ' + x + 'px; ';
+    line.setAttribute('style', styles);
+    return line;
+  }
+
+  private createLineElementByType(x, y, length, angle, type): HTMLDivElement{
+    var line = document.createElement("div");
+    var styles = 'border: 0.5pt ' + type + ' black; '
       + 'width: ' + length + 'px; '
       + 'height: 0; '
       + '-moz-transform: rotate(' + angle + 'rad); '
@@ -98,5 +114,22 @@ export class LineService {
     var alpha = Math.PI - Math.atan2(-b, a);
 
     return this.createLineElement(x, y, c, alpha);
+  }
+
+  public createLineByType(x1, y1, x2, y2, type) {
+    console.log( "(x1,y1) = (" + x1 + ',' + y1 + ') ; (x2,y2) = (' + x2 + ',' + y2 + ')');
+    var a = x1 - x2,
+      b = y1 - y2,
+      c = Math.sqrt(a * a + b * b);
+
+    var sx = (x1 + x2) / 2,
+      sy = (y1 + y2) / 2;
+
+    var x = sx - c / 2,
+      y = sy;
+
+    var alpha = Math.PI - Math.atan2(-b, a);
+
+    return this.createLineElementByType(x, y, c, alpha, type);
   }
 }
