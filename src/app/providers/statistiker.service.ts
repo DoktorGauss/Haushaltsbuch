2c9e0e5b288d8e995bb43b1f8b57489d0769e375
Dr Gauss <heilmann.sebastian@live.de>
2019-04-13 15:29:01 +0200

    #778 add mean, varianz, stdDeviation

--- src/app/providers/statistiker.service.ts
+++ src/app/providers/statistiker.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import * as ss from 'simple-statistics'
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,19 @@ import { Injectable } from '@angular/core';
 export class StatistikerService {
 
   constructor() { }
+
+  public calcMean(x : number[]){
+    return ss.mean(x);
+  }
+
+  public calcVar(x : number[]){
+    return ss.variance(x);
+  }
+
+  public calcStdDev(x: number[]){
+    return ss.standardDeviation(x);
+  }
+
+
+  
 }
