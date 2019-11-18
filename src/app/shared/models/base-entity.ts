5a60dcbff7bea78f62bfc9da18ca8911c3d55d96
Heilmann <s.heilmann@hensel-recycling.com>
2019-11-18 15:21:02 +0100

    #1000 BaseEntity

---
+++ src/app/shared/models/base-entity.ts
@@ -0,0 +1,18 @@
+import { PrimaryKey_ID } from './primary-key';
+import { SecondaryKey } from './secundary-key';
+
+/**
+* // TODO: all done
+* @class BaseAttribut for any Attributs out there with a zeahler to determine their density
+* @param name name for Debugging
+* @param value value 
+* @param zeahler how many BaseAttributs are out there?
+*/
+export class BaseEntity {
+    id: PrimaryKey_ID;
+    name: SecondaryKey<string>;
+    constructor(){
+        this.id = new PrimaryKey_ID();
+        this.name = new SecondaryKey<string>('BaseEntity');
+    }
+}
